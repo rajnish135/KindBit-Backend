315ef0875c1205e806e13eccc9322dca681a067d
rajnish135 <rajnishrajyan@gmail.com>
2025-07-13 22:37:58 +0530

    Initial commit2

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    // origin: 'https://kind-bite.vercel.app',
-    origin: 'http://localhost:5173', 
+    origin: 'https://kind-bite.vercel.app',
+    // origin: 'http://localhost:5173', 
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -33,8 +33,8 @@ app.set('io', io);
 
 // ✅ Middleware
 app.use(cors({
-  // origin: 'https://kind-bite.vercel.app',
-  origin: 'http://localhost:5173', 
+  origin: 'https://kind-bite.vercel.app',
+  // origin: 'http://localhost:5173', 
   credentials: true               
 }));
 
