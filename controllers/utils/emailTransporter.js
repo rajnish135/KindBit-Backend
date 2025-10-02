6bb2a2d6d897da8c07cea76fecafe7ac2770d6ab
rajnish135 <rajnishrajyan@gmail.com>
2025-10-02 21:57:00 +0530

    SMTPerrorfix

--- controllers/utils/emailTransporter.js
+++ controllers/utils/emailTransporter.js
@@ -1,9 +1,14 @@
 import nodemailer from 'nodemailer';  
 
- export const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS, // Use Gmail App Password, not your main password
-      }
+export const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS, // Gmail App Password
+  },
+  connectionTimeout: 20000,  // wait up to 20 seconds to connect
+  greetingTimeout: 20000,    // wait up to 20 seconds for SMTP greeting
+  socketTimeout: 20000,      // max time for any socket activity
 });
